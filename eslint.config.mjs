fd54ba629656c9ef51c262334c598a97c0878d43
augustoxd2343@gmail.com <augustoxd2343@gmail.com>
2025-12-06 12:37:53 -0300

    Actualizando Next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,15 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import nextVitals from "eslint-config-next/core-web-vitals.js";
+import {defineConfig,globalIgnores} from "eslint/config";
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  globalIgnores([
+    'next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ])
+]);
 
 export default eslintConfig;
