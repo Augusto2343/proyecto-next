6cf1c3c79f254d9c02650fc71aeff0674517becf
augustoxd2343@gmail.com <augustoxd2343@gmail.com>
2025-12-06 15:47:33 -0300

    Subiendo corrección de responsividad

--- app/components/ProductDetail.js
+++ app/components/ProductDetail.js
@@ -11,16 +11,16 @@ const ProductDetail = ({item}) =>{
             setVisible(false)
     }
     return(
-        <div className="conatiner">
+        <div className="conatiner ">
             <div className=" flex flex-row items-center justify-between w-screen mb-10 mt-10 p-10">
                 <Link href={"/productos/"+item.categoria} aria-label="Volver atrás">
                 <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
                 </svg>
                 </Link>
-                <h2 className="text-4xl text-gray-900 dark:text-white">{item.titulo}</h2>
+                <h2 className="text-xl text-gray-900 dark:text-white sm:text-4xl">{item.titulo}</h2>
             </div>
-            <div className="container flex flex-row gap-10">
+            <div className=" detalleProducto container flex flex-row gap-10">
                 <img src={item.imagen} />
                 <div className="flex flex-col">
                 <h2 className="text-2xl mb-10">{item.titulo}</h2>
